f3ae497c96d74b59928ecded36dc566378ca2336
Anand Raman <anand.raman@appdirect.com>
2018-03-25 12:43:13 -0700

    Iconlist updated

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
-import { Text, Container, Header, Content, Item, Input, Icon, Button } from 'native-base';
+import { Text, Container, Header, Item, Input, Icon } from 'native-base';
+import { IconList, IconListContainer } from './components/IconList';
+
+const icons = [
+  {
+    id: 1,
+    path: require('./icons/Animals.png'), // eslint-disable-line global-require
+    name: 'Animals',
+  },
+  {
+    id: 1,
+    path: require('./icons/Science.png'), // eslint-disable-line global-require
+    name: 'Science',
+  },
+  {
+    id: 1,
+    path: require('./icons/Sports.png'), // eslint-disable-line global-require
+    name: 'Sports',
+  },
+  {
+    id: 1,
+    path: require('./icons/Community.png'), // eslint-disable-line global-require
+    name: 'Community',
+  },
+];
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
@@ -23,7 +46,6 @@ export default class HomeScreen extends React.Component {
     return (
       <Container>
         <Header />
-
         <Item regular>
           <Icon name="ios-locate-outline" />
 
@@ -35,11 +57,11 @@ export default class HomeScreen extends React.Component {
             onSubmitEditing={this.handleSubmitEditing}
             blurOnSubmit
           />
-
         </Item>
-
+        <IconListContainer>
+          <IconList icons={icons} />
+        </IconListContainer>
         <Text>{this.state.text1}</Text>
-
         <Text>col2</Text>
 
 
